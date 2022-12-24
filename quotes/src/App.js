44f509914e0e9bc2ae4f9f72a2fff246d481d00d
poojachaturvedi <poojachaturvedi1672002@gmail.com>
2022-12-24 14:40:52 +0530

    changes_done

---
+++ quotes/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css'
+
+class App extends React.Component{
+    state={advice:' '};
+    componentDidMount(){
+        
+    }
+    fetchAdvice=()=>{
+        axios.get();
+    }
+    render(){
+        return(
+            <h1>App</h1>
+        );
+    }
+}
+export default App;
